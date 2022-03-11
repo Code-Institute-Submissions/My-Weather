ff82a979cdcb09f3f4d01765efb2e6af9abedc37
Gavin Shelley <Gavinshelley10@gmail.com>
2022-03-11 13:57:52 +0000

    Updated json api link

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,15 @@ const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 const locationPressureElement = document.querySelector(".location_pressure p");
-const locationTempMin = document.querySelector('.location_temp-min');
-const locationTempMax = document.querySelector('.location_temp-max');
-
-
+const locationTempMin = document.querySelector(".location_temp-min");
+const locationTempMax = document.querySelector(".location_temp-max");
 
 // App data
 const weather = {};
 
 weather.temperature = {
-    unit : "celsius"
-}
+  unit: "celsius",
+};
 
 // APP CONSTS AND VARS
 const KELVIN = 273;
@@ -23,67 +21,60 @@ const KELVIN = 273;
 const key = "82005d27a116c2880c8f0fcb866998a0";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
-}else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
 // SET USER'S POSITION
-function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    
-    getWeather(latitude, longitude);
+function setPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  getWeather(latitude, longitude);
 }
 
 // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
-function showError(error){
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+function showError(error) {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
 // GET WEATHER FROM API PROVIDER
-function getWeather(latitude, longitude){
-    //let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-    let api = `http://api.openweathermap.org/data/2.5/weather?q=london&appid=${key}`;
-    //https://api.openweathermap.org/data/2.5/weather?q=london&appid=4616b16851daa77e0e064e1b87acd6da&units=metric use this link to look at json
-    
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data){
-           weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-           weather.description = data.weather[0].description;
-           weather.iconId = data.weather[0].icon;
-           weather.city = data.name;
-           weather.country = data.sys.country;
-           weather.getPressure = data.main.pressure;
-           weather.getMinTemp = Math.floor(data.main.temp_min - KELVIN);
-           weather.getMaxTemp = Math.floor(data.main.temp_max - KELVIN);
+function getWeather(latitude, longitude) {
+  //let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  //https://api.openweathermap.org/data/2.5/weather?q=london&appid=4616b16851daa77e0e064e1b87acd6da&units=metric use this link to look at json
 
-           
-        })
-        .then(function(){
-            displayWeather();
-        });
+  fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+      weather.city = data.name;
+      weather.country = data.sys.country;
+      weather.getPressure = data.main.pressure;
+      weather.getMinTemp = Math.floor(data.main.temp_min - KELVIN);
+      weather.getMaxTemp = Math.floor(data.main.temp_max - KELVIN);
+    })
+    .then(function () {
+      displayWeather();
+    });
 }
 
-
-
-
-
 // DISPLAY WEATHER TO UI
-function displayWeather(){
-   iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-   tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-   descElement.innerHTML = weather.description;
-   locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-   locationPressureElement.innerHTML = `${weather.getPressure}<span> hPa</span>`;
-   locationTempMin.innerHTML = `${weather.getMinTemp}<span> Min Wind</span>`;
-   locationTempMax.innerHTML = `${weather.getMaxTemp}<span> Max Wind</span>`;
-
+function displayWeather() {
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+  locationPressureElement.innerHTML = `${weather.getPressure}<span> hPa</span>`;
+  locationTempMin.innerHTML = `${weather.getMinTemp}<span> Min Wind</span>`;
+  locationTempMax.innerHTML = `${weather.getMaxTemp}<span> Max Wind</span>`;
 }
